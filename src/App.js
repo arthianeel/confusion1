d21b64cefb4cd77fdfa3531077e000144b9e865b
Arthi <arthianeel2000@gmail.com>
2021-04-14 12:18:44 +0530

    react func part 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import Menu from './Components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      dishes: DISHES
+    };
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar dark color ="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
+        </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
     </div>
   );
+  }
 }
 
 export default App;
