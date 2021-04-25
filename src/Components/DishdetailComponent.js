c35d7c70469510d62bdc6d745a41fe7fcdcd9446
Arthi <arthianeel2000@gmail.com>
2021-04-25 22:25:48 +0530

    redux actions

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -27,9 +27,7 @@ class CommentForm extends Component {
       });
   }
   handleCommentFormSubmit(values) {
-    console.log("Current State is: " + JSON.stringify(values));
-    alert("Current State is: " + JSON.stringify(values));
-
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
     render(){
       return(
@@ -167,7 +165,7 @@ const DishDetail = (props) => {
       return (<div></div>);
   }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, dishId}){
     if (comments == null) {
         return (<div></div>);
     }
@@ -191,7 +189,7 @@ function RenderComments({comments}){
         <ul className='list-unstyled'>
                     {cmnt}
                 </ul>
-                <CommentForm dish={dish} comments={comments} />
+                <CommentForm dishId={dishId} addComment={addComment} comments={comments}/>
     </div>
     )
 }
@@ -216,7 +214,9 @@ function RenderComments({comments}){
 
           <div className='row'>
               <RenderDish dish={ props.dish } />
-              <RenderComments dish={props.dish} comments={ props.comments } />
+              <RenderComments  comments={ props.comments }
+              addComment={props.addComment}
+              dishId={props.dish.id} />
           </div>
 
 
